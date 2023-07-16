0b389c701618907e45494dbd8965bc059771eeac
JevTeo123 <123255675+JevTeo123@users.noreply.github.com>
2023-07-16 23:21:07 +0800

    Importing libraries

--- streamlit_asg.py
+++ streamlit_asg.py
@@ -1,8 +1,34 @@
 import streamlit as st
-import pandas
-import requests
-import snowflake.connector
-from urllib.error import URLError
+#import libs
+from snowflake.snowpark.session import Session
+import snowflake.snowpark.functions as F
+import snowflake.snowpark.types as T
+from snowflake.snowpark.window import Window
+import preprocessing
+
+import sys
+import getpass
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from matplotlib import pyplot as plt
+import seaborn as sns
+from mpl_toolkits import mplot3d 
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import NearestNeighbors
+from sklearn.decomposition import PCA
+from sklearn import tree
+
+from math import sqrt
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score, cross_validate
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+
+pd.set_option('display.max_columns', None)
 
 st.markdown("""
 <style>
