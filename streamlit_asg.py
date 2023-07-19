d5f10f8c40df737e44f7f8e2c3c0158ed761c738
JevTeo123 <123255675+JevTeo123@users.noreply.github.com>
2023-07-19 15:13:33 +0800

    Added input as well as main component

--- streamlit_asg.py
+++ streamlit_asg.py
@@ -3,19 +3,25 @@ import streamlit as st
 import pickle
 import numpy as np
 
-pd.set_option('display.max_columns', None)
-
-st.markdown("""
-<style>
-.big-font {
-    font-size:30px !important;
-}
-</style>
-""", unsafe_allow_html=True)
 
 model = pickle.load(open('model.pkl', 'rb'))
 
-st.markdown('<p class="big-font">Customer_RFM Model</p>', unsafe_allow_html=True)
-# Predictor Variables
-AVG_AMT = st.slider('Input Average Amount', 20, 58)
-AVG_QUANTITY = st.slider('Input Average Quantity', 3, 6)
+def predict_spend_rank(AVG_AMT, CITY, AVG_QUANTITY):
+    input = np.array([[AVG_AMT, CITY, AVG_QUANTITY]]).astype(np.float64)
+    prediction = model.predict(input)
+    return int(prediction)
+def main():
+    st.markdown("""
+    <style>
+    .big-font {
+        font-size:30px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+    st.markdown('<p class="big-font">Customer_RFM Model</p>', unsafe_allow_html=True)
+    # Predictor Variables
+    AVG_AMT = st.slider('Input Average Amount', 20, 58)
+    AVG_QUANTITY = st.slider('Input Average Quantity', 3, 6)
+
+if __name__ =='__main__':
+    main()
