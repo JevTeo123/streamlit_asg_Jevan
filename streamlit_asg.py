71e1e896b2d875d9c2d2b12a0a53eb9fcc90c357
JevTeo123 <123255675+JevTeo123@users.noreply.github.com>
2023-07-19 12:50:18 +0800

    Editing the code for pickle

--- streamlit_asg.py
+++ streamlit_asg.py
@@ -1,32 +1,7 @@
-import streamlit as st
 #import libs
-from snowflake.snowpark.session import Session
-import snowflake.snowpark.functions as F
-import snowflake.snowpark.types as T
-from snowflake.snowpark.window import Window
-import preprocessing
-
-import sys
-import getpass
-import pandas as pd
+import streamlit as st
+import pickle
 import numpy as np
-from datetime import datetime
-from matplotlib import pyplot as plt
-import seaborn as sns
-from mpl_toolkits import mplot3d 
-
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import NearestNeighbors
-from sklearn.decomposition import PCA
-from sklearn import tree
-
-from math import sqrt
-from sklearn import metrics
-from sklearn.model_selection import cross_val_score, cross_validate
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
 pd.set_option('display.max_columns', None)
 
@@ -45,14 +20,3 @@ AVG_QUANTITY = st.slider('Input Average Quantity', 3, 6)
 accountname = getpass.getpass('Enter account name') 
 username = getpass.getpass('Enter username')
 password = getpass.getpass('Enter password')
-connection_parameters = {
-    "account": accountname,
-    "user": username,
-    "password": password,
-    "role": "SYSADMIN",
-    "database": "FROSTBYTE_TASTY_BYTES",
-    "warehouse": "COMPUTE_WH",
-    "schema": "RAW_POS"
-}
-
-session = Session.builder.configs(connection_parameters).create()
