6bd0ff66ca99ab438abac38dedbb340400edd34e
JevTeo123 <123255675+JevTeo123@users.noreply.github.com>
2023-07-20 15:57:15 +0800

    Doing input and output
    Added additional dropdown input for city as well as the output for the prediction for the model

--- streamlit_asg.py
+++ streamlit_asg.py
@@ -22,7 +22,28 @@ def main():
     # Predictor Variables
     AVG_AMT = st.slider('Input Average Amount', 20, 58)
     AVG_QUANTITY = st.slider('Input Average Quantity', 3, 6)
-    CITY = st.selectbox('What is the city you want to predict customer's spend rank?', ('San Mateo', 'Denver', 'New York City', 'Seattle', 'Boston'))
+    option = st.selectbox(
+        'Input City',
+        ('San Mateo', 'Denver', 'New York City', 'Seattle', 'Boston'))
+    low_spender_html="""
+        <div style="background-color:#80ff80; padding:10px >
+        <h2 style="color:white;text-align:center;"> The customer is a low spender</h2>
+        </div>
+    """
+    high_spender_html="""
+        <div style="background-color:#F4D03F; padding:10px >
+        <h2 style="color:white;text-align:center;"> The customer is a high spender</h2>
+        </div>
+    """
+    
+    if st.button("Predict the spend rank of the customer"):
+        output = predict_spend_rank(AVG_AMT, CITY, AVG_QUANTITY)
+        st.success('The spend rank is {}'.format(output))
+
+        if output == 0:
+            st.markdown(low_spender_html, unsafe_allow_html=True)
+        elif output = 1:
+            st.markdown(high_spender_html, unsafe_allow_html=True)
     
 
 if __name__ =='__main__':
