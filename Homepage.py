8923adc2c74d7615d2fbf8eddfa0937e947e3f42
JevTeo123 <123255675+JevTeo123@users.noreply.github.com>
2023-08-03 00:46:33 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -12,3 +12,6 @@ st.set_page_config(
 )
 st.title("Main Page")
 st.sidebar.success("Select a page above.")
+
+st.markdown('<p class="big-font">Is your customer a high spender or a low spender?</p>', unsafe_allow_html=True)
+        st.markdown('<p class="normal-font">This customer segmentation model seeks to divide customers into distinct groups of individuals which in our case is whether a customer is a high or low spender. This will make it easier tp target specific groups of customers with tailored products so as to hit our high level goals.</p>', unsafe_allow_html=True)
