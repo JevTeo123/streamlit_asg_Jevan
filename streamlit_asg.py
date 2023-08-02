affee8db7d08e3334170b5b45d95c65ff034840f
JevTeo123 <123255675+JevTeo123@users.noreply.github.com>
2023-08-02 23:47:16 +0800

    Update streamlit_asg.py

--- streamlit_asg.py
+++ streamlit_asg.py
@@ -11,15 +11,4 @@ st.set_page_config(
             page_icon="Home"
 )
 st.title("Main Page")
-selected = option_menu(
-            menu_title=None,
-            options=["Home", "Customer Segmentation Model"],
-            icons=["house", "person-square"],
-            default_index = 0,
-            orientation="horizontal",
-            menu_icon="cast",
-        )
-if selected == "Home":
-    st.title(f"You have selected {selected}")
-if selected == "Customer Segmentation Model":
-        st.title(f"You have selected {selected}")
+st.sidebar.success("Select a page above.")
