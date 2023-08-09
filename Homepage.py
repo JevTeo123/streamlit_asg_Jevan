cef331f44b5395acfcab01693b275c69941bdd51
JevTeo123 <123255675+JevTeo123@users.noreply.github.com>
2023-08-09 12:21:13 +0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -84,8 +84,8 @@ if selected == "Customer Segmentation Model":
             (df['DAY_DIFF'] == DAY_DIFF) |
             (df['TOTAL_TRANS'] == TOTAL_TRANS)
             ]
-            #st.subheader("Filtered Data based on User Inputs:")
-            #st.write(filtered_df)
+            st.subheader("Filtered Data based on User Inputs:")
+            st.write(filtered_df)
             if st.button("Predict the spend rank of the customer🏆"):
                 input_data = np.asarray([CITY, GENDER, MARITAL_STATUS, CHILDREN_COUNT, AVG_AMT, AVG_QUANTITY, FREQ_SUBCAT, MEAN_PROFIT, DAY_DIFF, TOTAL_TRANS, AGE], dtype = np.float64)
                 output = predict_spend_rank(input_data)
